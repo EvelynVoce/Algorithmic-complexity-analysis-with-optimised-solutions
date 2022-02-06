bae24af3094d93a60bffa010496586e5647e8616
Evelyn Voce <74924781+EvelynVoce@users.noreply.github.com>
2022-02-06 16:38:54 +0000

    read from test file

---
+++ ASE-CW1/ASE-CW1/ASE-CW1.cpp
@@ -0,0 +1,36 @@
+// ASE-CW1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+int main()
+{
+
+    std::ifstream infile("input-pairs-20.txt");
+
+    if (!infile.good()) std::cout << "File not found" << std::endl;
+    else {
+        std::string sLine;
+        std::getline(infile, sLine);
+        std::cout << sLine << std::endl;
+        infile.close();
+    }
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+
+// io/read-file-sum.cpp - Read integers from file and print sum.
+// Fred Swartz 2003-08-20
