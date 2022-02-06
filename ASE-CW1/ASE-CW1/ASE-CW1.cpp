3b2b7d05303717d63de3f90c6bd3d1e2baff2fe8
Evelyn Voce <74924781+EvelynVoce@users.noreply.github.com>
2022-02-06 23:36:41 +0000

    Moved getting the unordered_map of all bricks into it's own function

--- ASE-CW1/ASE-CW1/ASE-CW1.cpp
+++ ASE-CW1/ASE-CW1/ASE-CW1.cpp
@@ -6,25 +6,37 @@
 #include <fstream>
 #include <unordered_map>
 
+std::unordered_map<std::string, std::string> get_all_bricks()
+{
+    std::unordered_map<std::string, std::string> brick_names;
+    std::ifstream infile("input-pairs-20.txt");
+    if (!infile.good()) throw "File not found";
+    std::string line;
+    while (std::getline(infile, line)) {
+        const size_t pos = line.find(",");
+        const std::string side1 = line.substr(0, pos);
+        const std::string side2 = line.substr(pos + 1);
+        brick_names.insert({ side1, side2 });
+    }
+    infile.close();
+    return brick_names;
+    
+}
+
 int main()
 {
+    std::unordered_map<std::string, std::string> brick_names = get_all_bricks();
+    auto const start_point = brick_names.begin();
+    std::list<std::string> result = { start_point->first, start_point->second};
 
-    std::ifstream infile("input-pairs-20.txt");
-
-    if (!infile.good()) std::cout << "File not found" << std::endl;
-    else {
-        std::unordered_map<std::string, std::string> brick_names;
-        std::string line;
-        while (std::getline(infile, line)) {
-            const size_t pos = line.find(",");
-            const std::string side1 = line.substr(0, pos);
-            const std::string side2 = line.substr(pos+1);
-            brick_names.insert({side1, side2});
-        }
-        infile.close();
-        for (auto const& pair : brick_names) {
-            std::cout << "{" << pair.first << ": " << pair.second << "}\n";
-        }
+    bool done = false;
+    while (!done) {
+        std::unordered_map<std::string, std::string>::const_iterator found_at = brick_names.find(result.back());
+        if (found_at == brick_names.end()) done = true;
+        else result.push_back(found_at->second);
     }
 
+    for (auto const& answers : result) {
+        std::cout << answers << std::endl;
+    }
 }
