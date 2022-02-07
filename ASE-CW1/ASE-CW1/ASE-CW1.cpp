7d02ab5e3fffdac76729ca3c9eaf49cea459e688
Evelyn Voce <74924781+EvelynVoce@users.noreply.github.com>
2022-02-07 22:56:05 +0000

    Updated file structure so all test files can be read

--- ASE-CW1/ASE-CW1/ASE-CW1.cpp
+++ ASE-CW1/ASE-CW1/ASE-CW1.cpp
@@ -10,7 +10,7 @@ struct brick_names_struct {
 brick_names_struct get_all_bricks()
 {
     brick_names_struct brick_names;
-    std::ifstream infile("input-pairs-20.txt");
+    std::ifstream infile("Great_Wall_Problem-test_data//20//input-pairs-20.txt");
     if (!infile.good()) throw "Error: File not found";
     std::string line;
     while (std::getline(infile, line)) {
@@ -27,7 +27,7 @@ brick_names_struct get_all_bricks()
 void update_western_wall(const std::unordered_map<std::string, std::string> brick_names, std::list<std::string>& result)
 {
     while (brick_names.find(result.front()) != brick_names.end()) {
-        std::unordered_map<std::string, std::string>::const_iterator found_at = brick_names.find(result.front());
+        const std::unordered_map<std::string, std::string>::const_iterator found_at = brick_names.find(result.front());
         result.push_front(found_at->second);
     }
 }
@@ -35,23 +35,39 @@ void update_western_wall(const std::unordered_map<std::string, std::string> bric
 void update_eastern_wall(const std::unordered_map<std::string, std::string> brick_names, std::list<std::string>& result)
 {
     while (brick_names.find(result.back()) != brick_names.end()) {
-        std::unordered_map<std::string, std::string>::const_iterator found_at = brick_names.find(result.back());
+        const std::unordered_map<std::string, std::string>::const_iterator found_at = brick_names.find(result.back());
         result.push_back(found_at->second);
     }
 }
 
 int main()
 {
-    brick_names_struct brick_names = get_all_bricks();
+    const brick_names_struct brick_names = get_all_bricks();
 
-    auto const start_point = brick_names.inorder.begin();
+    const std::unordered_map<std::string, std::string>::const_iterator start_point = brick_names.inorder.begin();
     std::list<std::string> result = { start_point->first, start_point->second};
 
     update_eastern_wall(brick_names.inorder, result);
     update_western_wall(brick_names.reverse_order, result);
     
-    std::cout << "Done" << std::endl;
-    for (auto const& answers : result) {
+    std::cout << "Done\n" << std::endl;
+    for (std::string const& answers : result) {
         std::cout << answers << std::endl;
     }
 }
+
+
+/* // Test to see if the need for reversed unordered map could be removed efficiently
+void update_western_wall(const std::unordered_map<std::string, std::string> brick_names, std::list<std::string>& result)
+{
+    bool keep_going = true;
+    while (keep_going) {
+        keep_going = false;
+        for (auto found_at = brick_names.begin(); found_at != brick_names.end(); ++found_at) {
+            if (found_at->second == result.front()) {
+                result.push_front(found_at->first);
+                keep_going = true;
+            }
+        }
+    }
+} */
